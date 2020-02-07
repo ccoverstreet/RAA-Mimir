c365cca5236d5df7f9bb2d24c0a9f36c7a8df2d4
ccoverstreet <coverst2@vols.utk.edu>
2020-02-07 15:39:04 -0500

    Added more printf statements

--- app/main.cpp
+++ app/main.cpp
@@ -1,13 +1,20 @@
+// RAA Mimir: Qt GUI Interface
+// Cale Overstreet
+// February 7, 2020
+// This is the main entrypoint for the RAA Mimir GUI Interface. I have decided to write the interface in C++ and Qt to make it more portable and memory efficient. The exact structure of the GUI program is still to be decided.
+
 #include <iostream>
 
 // Include Mimir Backend Library
 #include "../libmimirbackend/sources/mimirbackend.h"
 
 void test_mimirbackend() {
-	printf("Testing Mimir Backend...\n");
+	printf("\nTesting Mimir Backend...\n");
 
 	trajectorymodel x;
-	x.set_timestep_size(0.000001);
+
+	printf("\tSetting parameters...\n");
+	x.set_timestep_size(0.01);
 	x.set_horizontal_direction(0);
 	x.set_vertical_direction(80);
 	x.set_starting_altitude(0);
@@ -17,18 +24,29 @@ void test_mimirbackend() {
 	x.set_rocket_cd(0.2);
 	x.set_parachute_diameter(0.4);
 	x.set_parachute_cd(1.2);
+
+	printf("\tAdding Stage Info...\n");
 	x.pushback_stage_impulses(5);
 	x.pushback_stage_burn_times(2);
 	x.pushback_stage_delay_times(1000);
 	x.pushback_stage_total_masses(0.1);
 	x.pushback_stage_dry_masses(0.07);
+
+	printf("\tPreparing model...\n");
 	x.prepare_model();	
+
+	printf("\tRunning Trajectory Calculation...\n");
 	x.calculate_trajectory();
-	std::cout << x.get_apogee();
+	printf("\tFinished Calculation\n");
+	printf("\nApogee: %f\n", x.get_apogee());
 }
 
 int main() {
-	printf("Hello\n");
+	printf("RAA Mimir\n");
+	printf("Cale Overstreet\n");
+	printf("February 7, 2020\n");
+
+	// Test function to make sure library functions correctly
 	test_mimirbackend();
 	
 	return 0;
