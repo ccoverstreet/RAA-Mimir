04a53e982d18d53b29072596dee0d28b7f6f49ec
ccoverstreet <coverst2@vols.utk.edu>
2020-02-12 16:26:17 -0500

    Added toolbar:

--- app/sources/mainwindow.cpp
+++ app/sources/mainwindow.cpp
@@ -1,7 +1,20 @@
 #include "mainwindow.h"
 
+#include <QApplication>
+#include <QAction>
+#include <QMenu>
+#include <QMenuBar>
+
 
 raa_mainwindow::raa_mainwindow(QMainWindow* parent) : QMainWindow(parent) {
+	QAction* quit = new QAction("&Quit", this);
+	quit->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_Q));
+
+	QMenu* file_menu = menuBar()->addMenu("&File");
+	file_menu->addAction(quit);
+
+	connect(quit, &QAction::triggered, qApp, QApplication::quit);
+
 	container = new primarycontainer(this);
 	setCentralWidget(container);
 }
